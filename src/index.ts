95b9eee8af74845ac0c71d41e729777fecaefb67
benthumurigi <64170287+benthumurigi@users.noreply.github.com>
2024-01-15 21:09:42 +0000

    Changed Talent's service attribute to skills

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ interface Talent = {
     id: Principal,
     role: UserRole.TALENT,
     name: text,
-    services: Vec(text),
+    skills: Vec(text),
     hourlyRate: Opt(nat64),
     completedJobs: Vec(text),
     totalRating: Opt(nat64),
@@ -79,14 +79,6 @@ const Error = Variant({
 });
 
 export default Canister({
-    // Query calls complete quickly because they do not go through consensus
-    getMessage: query([], text, () => {
-        return message;
-    }),
-    // Update calls take a few seconds to complete
-    // This is because they persist state changes and go through consensus
-    setMessage: update([text], Void, (newMessage) => {
-        message = newMessage; // This change will be persisted
-    })
+    get
 });
 
